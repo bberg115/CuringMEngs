62eeb718b94fb909246d097c4a09b2c2f18245b1
bberg115 <ralphblumberg@gmail.com>
2018-10-14 22:21:35 -0500

    Add files via upload
    Changes on pi

---
+++ ralph_control.py
@@ -0,0 +1,233 @@
+# Ralph's edit.
+# take control.py and th_2.c 
+# output a the temp & rh to log
+# modify weemo based on being above or below targets
+# assumes that this require py2.7
+
+
+# Imports
+from subprocess import Popen, PIPE
+from os.path import expanduser
+from ouimeaux.environment import Environment
+from datetime import datetime, timedelta
+import xml.etree.cElementTree as ET
+
+# Functions
+def startWeMoEnvironment():
+    "Starts the WeMo environment"
+    env = Environment()
+    env.start()
+    return env
+
+def discoverWeMoDevices(env): #does this find more than 1?
+    "Discover WeMo devices"
+    env.discover(seconds=3)
+    return
+
+def connectToWeMo(env, switchName):
+    "Connects to the WeMo switch"
+    return env.get_switch(switchName)
+
+def isHumidifierRunning(switch):
+    "Checks if WeMo switch is on"
+    state = switch.basicevent.GetBinaryState()['BinaryState']
+    if state == '1' or state == '8':
+        return True
+    else:
+        return False
+
+def isHumidifierStopped(switch):
+    "Checks if WeMo switch is off"
+    if switch.basicevent.GetBinaryState()['BinaryState'] == '0':
+        return True
+    else:
+        return False
+
+def isHumidifierOutOfWater(switch): # still not sure if the switch we have will measure mWatts
+    "Checks if WeMo energy usage is low"
+    # Extract current energy usage in mW
+    usage = switch.insight.GetInsightParams()['InsightParams'].split("|")[7]
+    # Convert to watts
+    usage = int(usage) / 1000
+    # If watts is less than 10, it's out of water, unplugged, or turned off
+    if usage < 10:
+        return True
+    else:
+        return False
+
+def sendOutOfWaterAlert(): #see line 45
+    "Send an alert about humidifier being out of water"
+    # TODO
+    return
+
+def startHumidifier(switch):
+    "Turns the WeMo switch on"
+    switch.basicevent.SetBinaryState(BinaryState=1)
+    return
+
+def stopHumidifier(switch):
+    "Turns the WeMo switch off"
+    switch.basicevent.SetBinaryState(BinaryState=0)
+    return
+
+# Get home path
+home = "home/pi" # expanduser("~")   #assumes that this is in /pi/home? 
+
+
+
+# Read settings from XML file
+xmlPath = "/home/pi/raspi-rht/ralph_control.xml" #will pre-define characteristics / limits
+tree = ET.parse(xmlPath)
+root = tree.getroot()
+settingsXML = root.find("settings")
+statusXML = root.find("status")
+
+# Check if script is enabled. Quit if not.
+enabled = settingsXML.find("enabled").text
+if enabled != "True":
+    raise SystemExit(1)
+
+# Get the target humidity level
+targetRH = int(settingsXML.find("targetRH").text)
+# Get the tolerance for humidity level
+tolerance = int(settingsXML.find("tolerance").text)
+# Get time of day (24 hour format) in which the humidifier should start running
+startTime = settingsXML.find("startTime").text
+# Get how many hours it should run and convert to seconds
+runMinutes = int(settingsXML.find("runMinutes").text)
+# Get "friendly" name of WeMo Switch
+switchName = settingsXML.find("switchName").text
+
+# Calculate maximum humidity
+maxRH = targetRH + tolerance
+# Calculate minimum humidity
+minRH = targetRH - tolerance
+
+# Run program to get temp and humidity from sensor
+p = Popen(["sudo /home/pi/raspi-rht/./th_2"], stdout=PIPE, stderr=PIPE)
+output, err = p.communicate()
+
+# Check if an error was returned
+if err != '':
+    print "ERROR: th_2 returned error "+str(err)
+    # Exit with error status
+    raise SystemExit(1)
+
+# Split the output into separate variables
+temp, rh = output.split()
+
+# Make them floats
+temp = float(temp)
+rh = float(rh)
+
+# Set time format
+timeFormat = "%Y-%m-%d %H:%M:%S.%f"
+
+# Get current date and time as datetime object
+currentDateTime = datetime.now()
+
+# Start WeMo environment
+env = startWeMoEnvironment()
+
+# Check the last time we ran a WeMo discovery cycle
+lastDiscovery = statusXML.find("lastDiscovery").text
+
+if lastDiscovery is not None:
+    # Convert to datetime object
+    lastDiscovery = datetime.strptime(lastDiscovery, timeFormat)
+
+# Check if lastDiscovery is None or was more than 24 hours ago
+# this is only checking to see if the wemo still exists, not the values from rh/temp
+if lastDiscovery is None or (lastDiscovery + timedelta(hours=24)) < currentDateTime:
+    # Clear WeMo cache
+    p = Popen(["sudo", "wemo", "clear"], stdout=PIPE, stderr=PIPE)
+    p.communicate()
+    # Check if an error was returned
+    if err != '': print "WARNING: wemo clear returned error "+str(err)
+    # Rediscover devices
+    discoverWeMoDevices(env)
+    lastDiscovery = currentDateTime
+
+# Connect to WeMo Switch
+switch = connectToWeMo(env, switchName)
+
+# Status remains at 4 if no conditions are matched
+status = 4
+
+# Check if humidifier is running
+if isHumidifierRunning(switch): #if switch is on
+    # Check if humidifier is out of water
+    if isHumidifierOutOfWater(switch):
+        status = 2
+        friendlyStatus = "Out of Water"
+    else:
+        status = 1
+        friendlyStatus = "Running"
+# Check if humidifier is stopped
+elif isHumidifierStopped(switch):
+    status = 0
+    friendlyStatus = "Not Running"
+
+#print "Status = "+str(status)
+
+# If status is still 4, there's an issue reading the status
+if status == 4:
+    print "ERROR: Unable to read WeMo status"
+    # Exit with error status
+    raise SystemExit(1)
+
+# Get next time humidifier should start
+nextStart = statusXML.find("nextScheduledStart").text
+
+# If nextStart is None, assign to next start time on today's date
+if nextStart is None:
+    nextStart = currentDateTime.strftime("%Y-%m-%d")+" "+startTime+".000001"
+
+# Convert nextStart into datetime object
+nextStart = datetime.strptime(nextStart, timeFormat)
+
+# Get next time humidifier should stop
+nextStop = statusXML.find("nextScheduledStop").text
+
+# If nextStop is None, make it nextStart + runMinutes
+if nextStop is None:
+    nextStop = nextStart + timedelta(minutes=runMinutes) #change this to minutes???
+else:
+    # Convert nextStop into datetime object
+    nextStop = datetime.strptime(nextStop, timeFormat)
+
+# Check if start and stop times need to be updated
+# changed to minuites...
+if currentDateTime > nextStop:
+    nextStart = currentDateTime.strftime("%Y-%m-%d")+" "+startTime+".000001"
+    # Convert nextStart into datetime object
+    nextStart = datetime.strptime(nextStart, timeFormat)
+    if currentDateTime > nextStart:
+        # Add 24 hours to nextStart
+        nextStart = nextStart + timedelta(minutes=runMinutes)
+    nextStop = nextStart + timedelta(minutes=runMinutes)
+
+# Start or stop humidifier based on time and relative humidity
+if status == 0 and rh <= minRH and currentDateTime >= nextStart:
+    startHumidifier(switch)
+    friendlyStatus = "Running"
+    statusXML.find("startedDateTime").text = str(currentDateTime)
+    print str(currentDateTime)+"\tStarted\tTemp: "+str(temp)+"\tRH: "+str(rh)
+elif status > 0 and (rh >= maxRH or currentDateTime < nextStart):
+    stopHumidifier(switch)
+    friendlyStatus = "Not Running"
+    statusXML.find("stoppedDateTime").text = str(currentDateTime)
+    print str(currentDateTime)+"\tStopped\tTemp: "+str(temp)+"\tRH: "+str(rh)
+elif status == 2:
+    sendOutOfWaterAlert()
+    friendlyStatus = "Out of Water"
+
+# Update status in XML file
+statusXML.find("nextScheduledStart").text = str(nextStart)
+statusXML.find("nextScheduledStop").text = str(nextStop)
+statusXML.find("lastRH").text = str(rh)
+statusXML.find("lastTemp").text = str(temp)
+statusXML.find("lastUpdate").text = str(currentDateTime)
+statusXML.find("lastStatus").text = str(friendlyStatus)
+statusXML.find("lastDiscovery").text = str(lastDiscovery)
+tree.write(xmlPath)
