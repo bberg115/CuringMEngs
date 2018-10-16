096b6329d52035d1eea3aea178c63a484e291dc6
bberg115 <ralphblumberg@gmail.com>
2018-10-15 19:47:00 -0500

    Update ralph_control.py

--- ralph_control.py
+++ ralph_control.py
@@ -1,4 +1,4 @@
-# Ralph's edit. 1233 10/15/18
+# Ralph's edit. 2050 10/15/18
 #need to make it do RH first, then temp. Need to change functions to be more 'general' for both RH and Temp
 # take control.py and th_2.c 
 # output a the temp & rh to log
@@ -39,7 +39,7 @@ def connectToWeMo(env, switchName):
     "Connects to the WeMo switch"
     return env.get_switch(switchName)
 
-def isHumidifierRunning(switch):
+def isSwitchRunning(switch):
     "Checks if WeMo switch is on"
     state = switch.basicevent.GetBinaryState()['BinaryState']
     if state == '1' or state == '8':
@@ -47,14 +47,14 @@ def isHumidifierRunning(switch):
     else:
         return False
 
-def isHumidifierStopped(switch):
+def isSwitchStopped(switch):
     "Checks if WeMo switch is off"
     if switch.basicevent.GetBinaryState()['BinaryState'] == '0':
         return True
     else:
         return False
 
-def isHumidifierOutOfWater(switch): # still not sure if the switch we have will measure mWatts
+def isSwitchDrawingPower(switch): # still not sure if the switch we have will measure mWatts
     "Checks if WeMo energy usage is low"
     # Extract current energy usage in mW
     usage = switch.insight.GetInsightParams()['InsightParams'].split("|")[7]
@@ -66,28 +66,26 @@ def isHumidifierOutOfWater(switch): # still not sure if the switch we have will 
     else:
         return False
 
-def sendOutOfWaterAlert(): #see line 45
+def sendOutNoPowerAlert(): #see line 45
     "Send an alert about humidifier being out of water"
     # TODO
     return
 
-def startHumidifier(switch):
+def startSwitch(switch):
     "Turns the WeMo switch on"
     switch.basicevent.SetBinaryState(BinaryState=1)
     return
 
-def stopHumidifier(switch):
+def stopSwitch(switch):
     "Turns the WeMo switch off"
     switch.basicevent.SetBinaryState(BinaryState=0)
     return
 
-# Get home path
-home = "home/pi" # expanduser("~")   #assumes that this is in /pi/home? 
-
-
+env = startWeMoEnvironment()
+discoverWeMoDevices(env) #need to run once a day?
 
 # Read settings from XML file
-xmlPath = "/home/pi/raspi-rht/ralph_control.xml" #will pre-define characteristics / limits
+xmlPath = "ralph_control.xml" #will pre-define characteristics / limits
 tree = ET.parse(xmlPath)
 root = tree.getroot()
 settingsXML = root.find("settings")
@@ -104,13 +102,15 @@ targetRH = int(settingsXML.find("targetRH").text) # Get the target humidity leve
 toleranceRH = int(settingsXML.find("toleranceRH").text) # Get the tolerance for humidity level
 runMinutesRH = int(settingsXML.find("runMinutesRH").text) # Get the min time for Humidifier to run
 # may need to convert minutes to seconds.. like original script
-switchNameRH = settingsXML.find("switchNameRH").text # Get "friendly" name of WeMo Switch for humidifier
+switchName = settingsXML.find("switchNameRH").text # Get "friendly" name of WeMo Switch for humidifier
 
 # Calculate maximum humidity
 maxRH = targetRH + toleranceRH
 # Calculate minimum humidity
 minRH = targetRH - toleranceRH
 
+
+#temperature's turn, incomplete (need to add functions above)...
 #Get values for temperature from XML
 targetTemp = int(settingsXML.find("targetTemp").text) # Get the target temperature
 toleranceTemp = int(settingsXML.find("toleranceTemp").text) # Get the tolerance for temperature
@@ -119,9 +119,25 @@ runMinutesTemp = int(settingsXML.find("runMinutesTemp").text) # Get the min time
 switchNameTemp = settingsXML.find("switchNameTemp").text # Get "friendly" name of WeMo Switch for temperature
 
 # Calculate maximum temperature
-maxRH = targetTemp + toleranceTemp
+maxTemp = targetTemp + toleranceTemp
 # Calculate minimum temperature
-minRH = targetTemp - toleranceTemp
+minTemp = targetTemp - toleranceTemp
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -143,22 +159,23 @@ temp = float(temp)
 rh = float(rh)
 
 # Connect to WeMo Switch - RH First
-switchRH = connectToWeMo(env, switchNameRH)
+#change the following to loop between RH and Temp
+switch = connectToWeMo(env, switchName)
 
 # Status remains at 4 if no conditions are matched
 status = 4
 
 # Check if humidifier is running
-if isHumidifierRunning(switchRH): #if switch is on
+if isSwitchRunning(switch): #if switch is on
     # Check if humidifier is out of water
-    if isHumidifierOutOfWater(switchRH):
+    if isSwitchDrawingPower(switch):
         status = 2
         friendlyStatus = "Out of Water"
     else:
         status = 1
         friendlyStatus = "Running"
 # Check if humidifier is stopped
-elif isHumidifierStopped(switchRH):
+elif isSwitchStopped(switch):
     status = 0
     friendlyStatus = "Not Running"
 
@@ -173,24 +190,26 @@ if status == 4:
 
 # Start or stop humidifier based on time and relative humidity
 if status == 0 and rh <= minRH:
-    startHumidifier(switchRH)
+    startSwitch(switch)
     friendlyStatus = "Running"
 
 elif status > 0 and (rh >= maxRH):
-    stopHumidifier(switch)
+    stopSwitch(switch)
     friendlyStatus = "Not Running"
-    statusXML.find("stoppedDateTime").text = str(currentDateTime)
+#    statusXML.find("stoppedDateTime").text = str(currentDateTime)
     
 elif status == 2:
-    sendOutOfWaterAlert()
-    friendlyStatus = "Out of Water"
-
+    sendOutNoPowerAlert()
+    friendlyStatus = "No Power Draw"
+	
+	
+	
 # Update status in XML file
-statusXML.find("nextScheduledStart").text = str(nextStart)
-statusXML.find("nextScheduledStop").text = str(nextStop)
+#statusXML.find("nextScheduledStart").text = str(nextStart)
+#statusXML.find("nextScheduledStop").text = str(nextStop)
 statusXML.find("lastRH").text = str(rh)
 statusXML.find("lastTemp").text = str(temp)
-statusXML.find("lastUpdate").text = str(currentDateTime)
+#statusXML.find("lastUpdate").text = str(currentDateTime)
 statusXML.find("lastStatus").text = str(friendlyStatus)
-statusXML.find("lastDiscovery").text = str(lastDiscovery)
+#statusXML.find("lastDiscovery").text = str(lastDiscovery)
 tree.write(xmlPath)
